6e819bb134b842b76d42c960d4b394fcd748cea9
LazyKeru <killian.allaire@hotmail.com>
2023-12-06 00:58:50 +0100

    finished part 2

--- day5/main.go
+++ day5/main.go
@@ -86,37 +86,45 @@ func find_destination(source int, converter_maps []converter_map) int {
 	return destination
 }
 
-func destination_range(source_range location_range, converter_maps []converter_map, index int) []location_range {
-	var ranges_mapped  
+// i for converter_maps
+// k for map
+func destination_range(source_range location_range, converter_maps []converter_map, i int, k int) []location_range {
 	var destination_ranges []location_range
-	if index == len(converter_maps)-1 {
-		return append(destination_ranges, source_range)
-	}
-	for _, converter := range converter_maps[index].converters {
-		if converter.source_range_start <= source_range.start && source_range.end < converter.source_range_start+converter.range_length {
-			tmp1 := destination_range(location_range{source_range.start, source_range.end}, converter_maps, index+1)
+	if !(i == len(converter_maps) || k == len(converter_maps[i].converters)) {
+		map_source_start := converter_maps[i].converters[k].source_range_start
+		map_source_end := converter_maps[i].converters[k].source_range_start + converter_maps[i].converters[k].range_length - 1
+		map_destination_start := converter_maps[i].converters[k].destination_range_start
+		map_destination_end := converter_maps[i].converters[k].destination_range_start + converter_maps[i].converters[k].range_length - 1
+		if map_source_start <= source_range.start && source_range.end <= map_source_end {
+			tmp1 := destination_range(location_range{(source_range.start - map_source_start) + map_destination_start, (source_range.end - map_source_start) + map_destination_start}, converter_maps, i+1, 0)
 			destination_ranges = append(destination_ranges, tmp1...)
-		}
-		if source_range.start <= converter.source_range_start && converter.source_range_start <= source_range.end && source_range.end < converter.source_range_start+converter.range_length {
-			tmp1 := destination_range(location_range{converter.destination_range_start, (source_range.end - converter.source_range_start) + converter.destination_range_start}, converter_maps, index+1)
-			tmp2 := destination_range(location_range{source_range.start, converter.source_range_start - 1}, converter_maps, index)
-			destination_ranges = append(destination_ranges, tmp1...)
-			destination_ranges = append(destination_ranges, tmp2...)
-		}
-		if converter.source_range_start <= source_range.start && source_range.start < converter.source_range_start+converter.range_length && converter.source_range_start+converter.range_length < source_range.end {
-			tmp1 := destination_range(location_range{(source_range.start - converter.source_range_start) + converter.destination_range_start, converter.destination_range_start + converter.range_length - 1}, converter_maps, index+1)
-			tmp2 := destination_range(location_range{converter.source_range_start + converter.range_length + 1, source_range.end}, converter_maps, index)
+		} else if source_range.start <= map_source_start && map_source_start <= source_range.end && source_range.end <= map_source_end {
+			tmp1 := destination_range(location_range{map_destination_start, (source_range.end - map_source_start) + map_destination_start}, converter_maps, i+1, 0)
+			tmp2 := destination_range(location_range{source_range.start, map_source_start - 1}, converter_maps, i, k+1)
 			destination_ranges = append(destination_ranges, tmp1...)
 			destination_ranges = append(destination_ranges, tmp2...)
-		}
-		if source_range.start <= converter.source_range_start && converter.source_range_start+converter.range_length < source_range.end {
-			tmp1 := destination_range(location_range{converter.destination_range_start, converter.destination_range_start + converter.range_length - 1}, converter_maps, index+1)
-			tmp2 := destination_range(location_range{source_range.start, converter.source_range_start - 1}, converter_maps, index)
-			tmp3 := destination_range(location_range{converter.source_range_start + converter.range_length + 1, source_range.end}, converter_maps, index)
+		} else if map_source_start <= source_range.start && source_range.start <= map_source_end && map_source_end <= source_range.end {
+			tmp1 := destination_range(location_range{(source_range.start - map_source_start) + map_destination_start, map_destination_end}, converter_maps, i+1, 0)
+			tmp2 := destination_range(location_range{map_source_end + 1, source_range.end}, converter_maps, i, k+1)
+			destination_ranges = append(destination_ranges, tmp1...)
+			destination_ranges = append(destination_ranges, tmp2...)
+		} else if source_range.start <= map_source_start && map_source_end <= source_range.end {
+			tmp1 := destination_range(location_range{map_destination_start, map_destination_end}, converter_maps, i+1, 0)
+			tmp2 := destination_range(location_range{source_range.start, map_source_start - 1}, converter_maps, i, k+1)
+			tmp3 := destination_range(location_range{map_source_end + 1, source_range.end}, converter_maps, i, k+1)
 			destination_ranges = append(destination_ranges, tmp1...)
 			destination_ranges = append(destination_ranges, tmp2...)
 			destination_ranges = append(destination_ranges, tmp3...)
+		} else {
+			tmp1 := destination_range(location_range{source_range.start, source_range.end}, converter_maps, i, k+1)
+			destination_ranges = append(destination_ranges, tmp1...)
 		}
+	}
+	if i == len(converter_maps) {
+		destination_ranges = append(destination_ranges, source_range)
+	} else if k == len(converter_maps[i].converters) {
+		tmp1 := destination_range(location_range{source_range.start, source_range.end}, converter_maps, i+1, 0)
+		destination_ranges = append(destination_ranges, tmp1...)
 	}
 	return destination_ranges
 }
@@ -127,13 +135,13 @@ func locations_to_ranges(input locations) []location_range {
 	}
 	var output []location_range
 	for i := 0; i < len(input.location); i += 2 {
-		output = append(output, location_range{input.location[i], input.location[i] + input.location[i+1]})
+		output = append(output, location_range{input.location[i], input.location[i] + input.location[i+1] - 1})
 	}
 	return output
 }
 
 func main() {
-	seeds_locations, converter_maps := input("example.txt")
+	seeds_locations, converter_maps := input("input.txt")
 	var seed_destinations []int
 	for _, seed_location := range seeds_locations.location {
 		seed_destinations = append(seed_destinations, find_destination(seed_location, converter_maps))
@@ -148,6 +156,16 @@ func main() {
 	log.Print(smallest_destination)
 	log.Print("Everyone will starve we need more seeds")
 	locations_ranges := locations_to_ranges(seeds_locations)
-	log.Print(destination_range(locations_ranges[0], converter_maps, 0))
+	var destinations_ranges []location_range
+	for _, seed_location_range := range locations_ranges {
+		destinations_ranges = append(destinations_ranges, destination_range(seed_location_range, converter_maps, 0, 0)...)
+	}
+	smallest_destination = destinations_ranges[0].start
+	for i := 1; i < len(destinations_ranges); i++ {
+		if destinations_ranges[i].start < smallest_destination {
+			smallest_destination = destinations_ranges[i].start
+		}
+	}
 	log.Print("Part 2, lowest location :")
+	log.Print(smallest_destination)
 }
